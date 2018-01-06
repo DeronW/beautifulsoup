99c1474d74a9e6ecb98b78453e1891eeb6aaec52
DeronW <w.del@qq.com>
2018-01-06 22:23:17 +0800

    remove rst2pdf extension

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['rst2pdf.pdfbuilder']
+extensions = ['']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -332,7 +332,4 @@ texinfo_documents = [
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #
 # texinfo_no_detailmenu = False
-
-# for output PDF file
-pdf_documents = [('index', 'rst2pdf', 'BeautifulSoup Doc', 'Deron'),]
 
